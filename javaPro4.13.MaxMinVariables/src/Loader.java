c685851b5fa2c36e2a4f54884ec3fcbcdffff9ab
yawarwar <yawarwar@gmail.com>
2019-08-15 01:51:37 +0500

    Update 4 module

---
+++ javaPro4.13.MaxMinVariables/src/Loader.java
@@ -0,0 +1,33 @@
+public class Loader {
+    public static void main(String[] args) {
+        Integer intMaxValue = Integer.MAX_VALUE;
+        System.out.println(intMaxValue);
+        Integer intMinValue = Integer.MIN_VALUE;
+        System.out.println(intMinValue);
+
+        Float floatMaxValue = Float.MAX_VALUE;
+        System.out.println(floatMaxValue);
+        Float floatMinValue = Float.MIN_VALUE;
+        System.out.println(floatMinValue);
+
+        Double doubleMaxValue = Double.MAX_VALUE;
+        System.out.println(doubleMaxValue);
+        Double doubleMinValue = Double.MIN_VALUE;
+        System.out.println(doubleMinValue);
+
+        Long longMaxValue = Long.MAX_VALUE;
+        System.out.println(longMaxValue);
+        Long longMinValue = Long.MIN_VALUE;
+        System.out.println(longMinValue);
+
+        Byte byteMaxValue = Byte.MAX_VALUE;
+        System.out.println(byteMaxValue);
+        Byte byteMinValue = Byte.MIN_VALUE;
+        System.out.println(byteMinValue);
+
+        Short shortMaxValue = Short.MAX_VALUE;
+        System.out.println(shortMaxValue);
+        Short shortMinValue = Short.MIN_VALUE;
+        System.out.println(shortMinValue);
+    }
+}
