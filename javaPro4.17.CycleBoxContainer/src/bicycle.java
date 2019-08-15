c6df41b82447e8db08c3586018cea04d5e51edb1
yawarwar <yawarwar@gmail.com>
2019-08-15 16:04:32 +0500

    Update 4 module

---
+++ javaPro4.17.CycleBoxContainer/src/bicycle.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bicycle {
+    public static void main(String[] args) throws IOException {
+        for (; ; ) {
+            System.out.println("\nВведите количество контейнеров для отправки");
+
+            int fullBoxs = Integer.parseInt((new BufferedReader(new InputStreamReader(System.in))).readLine());
+            int fullContainers = (int) Math.ceil(fullBoxs / 27.0);
+            int fullTrucks = (int) Math.ceil(fullContainers / 12.0);
+
+            for (int truck = 1; truck <= fullTrucks; truck++) {
+                System.out.println("\nГрузовик " + truck + ":");
+                for (int container = 1; container <= 12 && fullContainers >= 1; container++, fullContainers--) {
+                    System.out.println("\nКонтейнер " + container + ":");
+                    for (int box = 1; box <= 27 && fullBoxs >= 1; box++, fullBoxs--) {
+                        System.out.println("\nЯщик " + box);
+                    }
+                }
+            }
+        }
+    }
+}
+
