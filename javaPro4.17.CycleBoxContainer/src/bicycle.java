34f39903df11588f818a2c78723bf17493160680
yawarwar <yawarwar@gmail.com>
2019-08-16 19:38:42 +0500

    Update 4 module 4.17 homework

--- javaPro4.17.CycleBoxContainer/src/bicycle.java
+++ javaPro4.17.CycleBoxContainer/src/bicycle.java
@@ -10,13 +10,14 @@ public class bicycle {
             int fullBoxs = Integer.parseInt((new BufferedReader(new InputStreamReader(System.in))).readLine());
             int fullContainers = (int) Math.ceil(fullBoxs / 27.0);
             int fullTrucks = (int) Math.ceil(fullContainers / 12.0);
+            int countBoxs = 1;
 
             for (int truck = 1; truck <= fullTrucks; truck++) {
                 System.out.println("\nГрузовик " + truck + ":");
                 for (int container = 1; container <= 12 && fullContainers >= 1; container++, fullContainers--) {
                     System.out.println("\nКонтейнер " + container + ":");
-                    for (int box = 1; box <= 27 && fullBoxs >= 1; box++, fullBoxs--) {
-                        System.out.println("\nЯщик " + box);
+                    for (int box = 1; box <= 27 && countBoxs <= fullBoxs; box++, countBoxs++) {
+                        System.out.println("\nЯщик " + countBoxs);
                     }
                 }
             }
