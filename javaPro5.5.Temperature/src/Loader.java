cf2b0d94fff81ae374c5717777215adccc773ad2
yawarwar <yawarwar@gmail.com>
2019-08-22 13:23:38 +0500

    Update 5 module fix Temperature 5.5 homework

--- javaPro5.5.Temperature/src/Loader.java
+++ javaPro5.5.Temperature/src/Loader.java
@@ -1,23 +1,26 @@
 public class Loader {
 
-    private static int patients = 30;
-    private static int startTemperature = 32;
-    private static int endTemperature = 40;
-    private static double minNormalTemperature = 36.2;
-    private static double maxNormalTemperature = 36.9;
-    private static float[] temperatures = new float[patients];
+    private static final int PATIENTS = 30;
+    private static final int START_TEMPERATURE = 32;
+    private static final int END_TEMPERATURE = 40;
+    private static final double MIN_NORMAL_TEMPERATURE = 36.2;
+    private static final double MAX_NORMAL_TEMPERATURE = 36.9;
 
     public static void main(String[] args) {
+        float[] temperatures = new float[PATIENTS];
         float sumTemperatures = 0;
         float averageTemperature = 0;
+        int countTruePatients = 0;
         for (int i = 0; i < temperatures.length; i++) {
-            temperatures[i] = (float) Math.random() * (endTemperature - startTemperature) + startTemperature;
-            if (temperatures[i] > minNormalTemperature && temperatures[i] < maxNormalTemperature) {
+            temperatures[i] = (float) Math.random() * (END_TEMPERATURE - START_TEMPERATURE) + START_TEMPERATURE;
+            if (temperatures[i] > MIN_NORMAL_TEMPERATURE && temperatures[i] < MAX_NORMAL_TEMPERATURE) {
                 System.out.println("Пациент № " + (i + 1) + " с температурой " + temperatures[i] + " \"ЗДОРОВ\"");
+                countTruePatients += 1;
             }
             sumTemperatures += temperatures[i];
             averageTemperature = sumTemperatures / (i + 1);
         }
+        System.out.println("Итого здоровых: " + countTruePatients);
         System.out.println("Средняя температура по больнице: " + averageTemperature);
     }
 }
