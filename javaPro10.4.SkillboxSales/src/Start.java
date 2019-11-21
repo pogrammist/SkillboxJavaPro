cc189f896dd3293be27290cd69d67c64e529b8ff
yawarwar <yawarwar@gmail.com>
2019-11-21 15:31:24 +0500

    Add 10 module fix SkillboxSales 10.4 homework

--- javaPro10.4.SkillboxSales/src/Start.java
+++ javaPro10.4.SkillboxSales/src/Start.java
@@ -8,11 +8,11 @@ public class Start {
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT CONCAT(name, ' - ', count(*)/12) " +
-                    "AS \"subscription average count of courses\" " +
+            ResultSet resultSet = statement.executeQuery("SELECT CONCAT(name, ' - ', count(*)" +
+                    "/count(distinct month(subscription_date))) AS \"subscription average count of courses in month\" " +
                     "FROM Courses JOIN Subscriptions ON id = course_id group by month(course_id), name");
             while (resultSet.next()) {
-                String courseName = resultSet.getString("subscription average count of courses");
+                String courseName = resultSet.getString("subscription average count of courses in month");
                 System.out.println(courseName);
             }
             resultSet.close();
